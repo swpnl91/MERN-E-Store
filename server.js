df8dbbfcca223656911aeacc7cb7a90bf1e5f5a0
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-04 07:52:36 -0500

    setup .env file

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from "express";      // using 'import' instead of 'const... = require(...)' by changing 'type' to 'module' in package.json. It's 'commonjs' by default for you to be able to use 'const...require'
 import colors from "colors";
+import dotenv from "dotenv";
 
 
 
+//Configure env
+dotenv.config();   // since .env file is in the root folder we don't need to define a path like this 'dotenv.config({path:'...'});'
 
 
 //Rest object
@@ -17,12 +20,12 @@ app.get("/", (req, res) => {
 });
 
 //PORT
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;    // 'process' comes with node.js by default
 
 //Run server/Listen
 app.listen(PORT, () => {
   console.log(
-    `Server Running on port ${PORT}`.bgCyan
+    `Server Running on port ${PORT}`.bgCyan     // .bgCyan.white comes from the 'colors' package
       .white
   );
 });
