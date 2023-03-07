5fa732bfe07f74f2623046c4ae73fd4f61015899
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-07 14:39:39 -0500

    create and add logic to submit handler function in Login.js

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useNavigate, useLocation } from "react-router-dom";
 import toast from "react-hot-toast";
 import "../../styles/AuthStyles.css";
-
+import { useAuth } from "../../context/auth";
 
 
 
@@ -16,6 +16,32 @@ const Login = () => {
 
   const navigate = useNavigate();
   const location = useLocation();
+
+  // Form Submit Handler Function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/api/v1/auth/login", {
+        email,
+        password,
+      });
+      if (res && res.data.success) {
+        toast.success(res.data && res.data.message);
+        setAuth({
+          ...auth,
+          user: res.data.user,
+          token: res.data.token,
+        });
+        localStorage.setItem("auth", JSON.stringify(res.data));
+        navigate(location.state || "/");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };  
 
 
   return (
