03511ea1b6e5c8b41556c89ba660a7ed52c665e3
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-07 06:27:27 -0500

    create Contact.js

---
+++ client/src/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+
+
+const Contact = () => {
+  return (
+    <Layout>
+      
+    </Layout>
+  );
+};
+
+export default Contact;
