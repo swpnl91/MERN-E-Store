e57285e2ee8bd8bd819195b43c58ad2132bf9f91
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-07 06:30:55 -0500

    create Pagenotfound.js

---
+++ client/src/pages/Pagenotfound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "./../components/Layout/Layout";
+
+const Pagenotfound = () => {
+  return (
+    <Layout>
+      
+    </Layout>
+  );
+};
+
+export default Pagenotfound;
