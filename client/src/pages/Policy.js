37020579814a3be7b248acf744aed91b122af3c2
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-07 12:02:03 -0500

    add structure for Policy.js

--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -4,7 +4,32 @@ import Layout from "./../components/Layout/Layout";
 const Policy = () => {
   return (
     <Layout>
-      
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/privacypolicy.jpeg"
+            alt="privacypolicy"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          {/* <p>#1 privacy policy</p>
+          <p>#2 privacy policy</p>
+          <p>#3 privacy policy</p>
+          <p>#4 privacy policy</p>
+          <p>#5 privacy policy</p>
+          <p>#6 privacy policy</p>
+          <p>#7 privacy policy</p> */}
+          <p className="text-justify mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            officiis obcaecati esse tempore unde ratione, eveniet mollitia,
+            perferendis eius temporibus dicta blanditiis doloremque explicabo
+            quasi sunt vero optio cum aperiam vel consectetur! Laborum enim
+            accusantium atque, excepturi sapiente amet! Tenetur ducimus aut
+            commodi illum quidem neque tempora nam.
+          </p>
+        </div>
+      </div>
     </Layout>
   );
 };
