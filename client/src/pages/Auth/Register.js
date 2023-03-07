cd6f55c37ded15e7b4d094792fe9cfb07b8f0c29
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-07 12:35:22 -0500

    create Register.js

---
+++ client/src/pages/Auth/Register.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import Layout from "./../../components/Layout/Layout";
+
+import "../../styles/AuthStyles.css";
+
+
+
+const Register = () => {
+  
+  
+  return (
+    <Layout title="Register - E-Store">
+      
+      
+    </Layout>
+  );
+};
+
+export default Register;
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -6,7 +6,7 @@ import Layout from "./../components/Layout/Layout";
 const HomePage = () => {
   
   return (
-    <Layout>
+    <Layout title={"Our Products - Best offers!"}>
       
     </Layout>
   );
