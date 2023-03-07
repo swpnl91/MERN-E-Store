bfbaf5b9d6bd812f5f66a74c38418e540864bdbf
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-07 12:36:57 -0500

    add route for Register.js in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Policy from "./pages/Policy";
 import Pagenotfound from "./pages/Pagenotfound";
+import Register from "./pages/Auth/Register";
+
+
 
 
 function App() {
@@ -12,6 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
 
+
+        <Route path="/register" element={<Register />} />
 
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
