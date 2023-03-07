bcb0169710936295a978e19d7f63d3ff8778558e
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-07 14:41:36 -0500

    add route for Login.js in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Contact from "./pages/Contact";
 import Policy from "./pages/Policy";
 import Pagenotfound from "./pages/Pagenotfound";
 import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
 
 
 
@@ -18,6 +19,7 @@ function App() {
 
         <Route path="/register" element={<Register />} />
 
+        <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/policy" element={<Policy />} />
