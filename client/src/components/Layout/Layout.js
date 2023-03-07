d0a6582d2d787004ed311105a22a39f116ea4247
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-07 13:24:42 -0500

    check .env file

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -19,6 +19,7 @@ const Layout = ({ children, title, description, keywords, author }) => {
       <Header />
       <main style={{ minHeight: "70vh" }}>
         <Toaster />
+        
         {children}
       </main>
       <Footer />
