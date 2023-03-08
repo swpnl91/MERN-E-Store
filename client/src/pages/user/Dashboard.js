8b5fffbdf09a74519e88cc38efe44848fa942cff
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-08 08:36:49 -0500

    create Dashboard.js

---
+++ client/src/pages/user/Dashboard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+
+
+
+
+const Dashboard = () => {
+  
+  
+  return (
+    <Layout title={"Dashboard - E-Store"}>
+      
+    </Layout>
+  );
+};
+
+export default Dashboard;
