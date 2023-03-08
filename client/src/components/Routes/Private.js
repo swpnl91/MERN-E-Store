7384901a14337dc5befdb8a139f81df982b5a84b
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-08 09:34:36 -0500

    add useEffect functionality in Private.js

--- client/src/components/Routes/Private.js
+++ client/src/components/Routes/Private.js
@@ -1,8 +1,28 @@
 import { useState, useEffect } from "react";
 import { useAuth } from "../../context/auth";
+import { Outlet } from "react-router-dom";
+import axios from "axios";
 
 
 export default function PrivateRoute() {
-  const [ok, setOk] = useState(false);
+  
+  const [ok, setOk] = useState(false);    // 'ok'/'setOk' because we send 'ok: true' in backend in 'authRoute.js'
   const [auth, setAuth] = useAuth();
+
+  useEffect(() => {
+    const authCheck = async () => {
+      const res = await axios.get("/api/v1/auth/user-auth");
+      if (res.data.ok) {
+        setOk(true);
+      } else {
+        setOk(false);
+      }
+    };
+
+    if (auth?.token) authCheck();       // 'auth?.token' means - if 'auth' exists AND in that do we have 'auth.token'? Only then call 'authCheck()'.
+
+  }, [auth?.token]);
+
+  return ok ? <Outlet /> : '<Spinner />';    // '<Outlet />' is used for nested Routing (Can be seen in 'App.js')
+
 }
