88c770e55c38bd6d5a397c686de37f796957dcf8
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-08 11:37:29 -0500

    add structure to ForgotPassword.js

--- client/src/pages/Auth/ForgotPassword.js
+++ client/src/pages/Auth/ForgotPassword.js
@@ -8,9 +8,53 @@ import "../../styles/AuthStyles.css";
 const ForgotPassword = () => {
   
   
+
+  
   return (
     <Layout title={"Forgot Password - E-Store"}>
-      <h1>hooba</h1>
+      <div className="form-container ">
+        <form onSubmit={handleSubmit}>
+          <h4 className="title">RESET PASSWORD</h4>
+
+          <div className="mb-3">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Email "
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Favorite Sports Team"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter New Password"
+              required
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            RESET
+          </button>
+        </form>
+      </div>
     </Layout>
   );
 };
