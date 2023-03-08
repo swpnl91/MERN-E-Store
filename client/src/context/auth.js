e10fd63aac185830b05fe4dd270f189f387601e1
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-08 06:39:14 -0500

    export custom hook and AuthProvider

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -11,5 +11,15 @@ const AuthProvider = ({ children }) => {
     token: "",
   });
 
-  
+  return (
+    <AuthContext.Provider value={[auth, setAuth]}>
+      {children}
+    </AuthContext.Provider>
+  );
+
 };
+
+// Custom Hook
+const useAuth = () => useContext(AuthContext);
+
+export { useAuth, AuthProvider };
