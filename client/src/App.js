6d0f3bf039e9439a91fe11d7da704d07d81cb57e
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-08 11:34:56 -0500

    import packages/files in ForgotPassword.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Register from "./pages/Auth/Register";
 import Login from "./pages/Auth/Login";
 import Dashboard from "./pages/user/Dashboard";
 import PrivateRoute from "./components/Routes/Private";
-import ForgotPasssword from "./pages/Auth/ForgotPasssword";
+import ForgotPassword from "./pages/Auth/ForgotPassword";
 
 
 
@@ -27,7 +27,7 @@ function App() {
         
 
         <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPasssword />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
---
+++ client/src/pages/Auth/ForgotPassword.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import Layout from "../../components/Layout/Layout";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import "../../styles/AuthStyles.css";
+
+const ForgotPassword = () => {
+  
+  
+  return (
+    <Layout title={"Forgot Password - E-Store"}>
+      <h1>hooba</h1>
+    </Layout>
+  );
+};
+
+export default ForgotPassword;
