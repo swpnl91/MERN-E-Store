afeec54d57a1e09dc60507cb21866219b1320688
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-08 09:05:56 -0500

    add routing for protected/authorized user paths in backend

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -24,5 +24,10 @@ router.post("/login", loginController);
 // Test routes
 router.get("/test", requireSignIn, isAdmin, testController);     // The 'next()' in 'requireSignIn'/'isAdmin' (middlewares) makes it execute 'testController'
 
+// Routing for Protected/Authorized paths (User); Method - GET
+router.get("/user-auth", requireSignIn, (req, res) => {     // not creating a separate controller as it's pretty straightforward
+  res.status(200).send({ ok: true });
+});
+
 
 export default router;
