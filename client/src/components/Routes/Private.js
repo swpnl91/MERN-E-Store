8468a0ecad0fa122c23c9338300666bb2f470f21
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-08 09:11:52 -0500

    add states Private.js

--- client/src/components/Routes/Private.js
+++ client/src/components/Routes/Private.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
+import { useAuth } from "../../context/auth";
 
 
 export default function PrivateRoute() {
-  
+  const [ok, setOk] = useState(false);
+  const [auth, setAuth] = useAuth();
 }
