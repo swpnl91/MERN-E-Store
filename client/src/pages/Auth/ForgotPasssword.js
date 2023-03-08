ed977e69682cd33901f03e164d8b989ae9e4e83f
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-08 11:27:14 -0500

    create ForgotPasssword.js

---
+++ client/src/pages/Auth/ForgotPasssword.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Layout from "./../../components/Layout/Layout";
+
+
+import "../../styles/AuthStyles.css";
+
+const ForgotPasssword = () => {
+  
+  
+  return (
+    <Layout title={"Forgot Password - E-Store"}>
+      
+    </Layout>
+  );
+};
+
+export default ForgotPasssword;
