925ffc0ef8da3e4f9abcc0e251a485b090d9bee7
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-09 09:05:23 -0500

    add AdminMenu in CreateProduct.js

--- client/src/pages/Admin/CreateProduct.js
+++ client/src/pages/Admin/CreateProduct.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./../../components/Layout/Layout";
+import AdminMenu from "./../../components/Layout/AdminMenu";
+
 
 
 const CreateProduct = () => {
