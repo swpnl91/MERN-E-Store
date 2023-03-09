9a27d2974b1c178cbcfd66aba23f68816aeac607
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-09 09:11:42 -0500

    add structure to  UserMenu.js

--- client/src/components/Layout/UserMenu.js
+++ client/src/components/Layout/UserMenu.js
@@ -7,7 +7,23 @@ const UserMenu = () => {
   
   return (
     <div>
-      
+      <div className="text-center dashboard-menu">
+        <div className="list-group">
+          <h4>Dashboard</h4>
+          <NavLink
+            to="/dashboard/user/profile"
+            className="list-group-item list-group-item-action"
+          >
+            Profile
+          </NavLink>
+          <NavLink
+            to="/dashboard/user/orders"
+            className="list-group-item list-group-item-action"
+          >
+            Orders
+          </NavLink>
+        </div>
+      </div>
     </div>
   );
 };
