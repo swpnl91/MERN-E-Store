fce1dfbca6b9a7669b8eea4e5aff72abb0dbde2e
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-09 09:15:44 -0500

    create Orders.js

---
+++ client/src/pages/user/Orders.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+import Layout from "./../../components/Layout/Layout";
+
+
+
+const Orders = () => {
+  
+  
+  
+  
+  return (
+    <Layout title={"Your Orders"}>
+      
+    </Layout>
+  );
+};
+
+export default Orders;
