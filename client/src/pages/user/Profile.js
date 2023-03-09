ae04ef8f6031f55eec190f712c3098e8a272b820
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-09 09:19:55 -0500

    import required files/packages in Profile.js

--- client/src/pages/user/Profile.js
+++ client/src/pages/user/Profile.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
-
+import UserMenu from "../../components/Layout/UserMenu";
 import Layout from "./../../components/Layout/Layout";
-
+import { useAuth } from "../../context/auth";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 
 
