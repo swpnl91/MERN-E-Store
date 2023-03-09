caca19cfbb8e025101fb427089a25c60128cf85b
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-09 11:50:53 -0500

    create productController.js

---
+++ controllers/productController.js
@@ -0,0 +1,11 @@
+import productModel from "../models/productModel.js";
+import categoryModel from "../models/categoryModel.js";
+
+
+
+import slugify from "slugify";
+
+import dotenv from "dotenv";
+
+
+dotenv.config();
