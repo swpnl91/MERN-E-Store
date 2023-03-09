185ea9184cacc0547dbf732d3690d2eb6e45ef9d
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-09 11:49:03 -0500

    create productRoutes.js

---
+++ routes/productRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+
+
+
+
+const router = express.Router();
+
+
+// ROUTES
+
+
+
+
+
+
+
+
+export default router;
