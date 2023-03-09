3b47d58a0350227f96dcb0dc9d616ac1493dc344
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-09 09:31:43 -0500

    add functionality/logic to Orders.js

--- client/src/pages/user/Orders.js
+++ client/src/pages/user/Orders.js
@@ -3,12 +3,26 @@ import UserMenu from "../../components/Layout/UserMenu";
 import Layout from "./../../components/Layout/Layout";
 import axios from "axios";
 import { useAuth } from "../../context/auth";
-
+import moment from "moment";
 
 
 const Orders = () => {
   
+  const [orders, setOrders] = useState([]);
+  const [auth, setAuth] = useAuth();
   
+  const getOrders = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/auth/orders");
+      setOrders(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (auth?.token) getOrders();
+  }, [auth?.token]);
   
   
   return (
