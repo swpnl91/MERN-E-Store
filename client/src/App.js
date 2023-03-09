bc26c215ec0e726289e760cc0341cac39147540c
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-09 08:38:33 -0500

    add route for Users in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import AdminRoute from "./components/Routes/AdminRoute";
 import AdminDashboard from "./pages/Admin/AdminDashboard";
 import CreateCategory from "./pages/Admin/CreateCategory";
 import CreateProduct from "./pages/Admin/CreateProduct";
+import Users from "./pages/Admin/Users";
 
 
 
@@ -34,6 +35,9 @@ function App() {
           <Route path="admin" element={<AdminDashboard />} />
           <Route path="admin/create-category" element={<CreateCategory />} />
           <Route path="admin/create-product" element={<CreateProduct />} />
+
+
+          <Route path="admin/users" element={<Users />} />
 
         </Route>
         
