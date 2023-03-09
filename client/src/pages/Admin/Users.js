09b0febff946a698fa5870b3247599ba1a0af97a
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-09 08:37:15 -0500

    create Users.js

---
+++ client/src/pages/Admin/Users.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Layout from "./../../components/Layout/Layout";
+
+const Users = () => {
+  
+  return (
+    <Layout title={"Dashboard - All Users"}>
+      
+    </Layout>
+  );
+};
+
+export default Users;
