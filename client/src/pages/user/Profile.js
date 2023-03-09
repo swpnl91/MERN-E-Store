2c971938c45d39e335caa6c1a86c034b1633f387
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-09 09:22:14 -0500

    add structure to Profile.js

--- client/src/pages/user/Profile.js
+++ client/src/pages/user/Profile.js
@@ -14,7 +14,86 @@ const Profile = () => {
 
   return (
     <Layout title={"Your Profile"}>
-      
+      <div className="container-fluid m-3 p-3 dashboard">
+        <div className="row">
+          
+          <div className="col-md-3">
+            <UserMenu />
+          </div>
+
+          <div className="col-md-8">
+            <div className="form-container" style={{ marginTop: "-40px" }}>
+              
+              <form onSubmit={handleSubmit}>
+                
+                <h4 className="title">USER PROFILE</h4>
+                
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter Your Name"
+                    autoFocus
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter Your Email "
+                    disabled
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    placeholder="Enter Your Password"
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter Your Phone #"
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter Your Address"
+                  />
+                </div>
+
+                <button type="submit" className="btn btn-primary">
+                  UPDATE
+                </button>
+                
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
