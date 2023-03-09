b43dc8893b5ee28e1c187821fb09d816962bafee
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-09 08:06:53 -0500

    add structure to AdminMenu.js

--- client/src/components/Layout/AdminMenu.js
+++ client/src/components/Layout/AdminMenu.js
@@ -6,7 +6,38 @@ import React from "react";
 const AdminMenu = () => {
   return (
     <>
-      
+      {/* Ensure that the bootstrap HTML code is converted to JSX */}
+      <div className="text-center">
+        <div className="list-group dashboard-menu">
+          <h4>Admin Panel</h4>
+          <NavLink
+            to="/dashboard/admin/create-category"
+            className="list-group-item list-group-item-action"
+          >
+            Create Category
+          </NavLink>
+          <NavLink
+            to="/dashboard/admin/create-product"
+            className="list-group-item list-group-item-action"
+          >
+            Create Product
+          </NavLink>
+          <NavLink
+            to="/dashboard/admin/products"
+            className="list-group-item list-group-item-action"
+          >
+            Products
+          </NavLink>
+          <NavLink
+            to="/dashboard/admin/orders"
+            className="list-group-item list-group-item-action"
+          >
+            Orders
+          </NavLink>
+          
+          
+        </div>
+      </div>
     </>
   );
 };
