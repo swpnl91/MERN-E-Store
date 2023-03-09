5d4a8aba7c8f192a2957deeed4fbe1bbab21bc0d
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-09 08:32:11 -0500

    create CreateProduct.js

---
+++ client/src/pages/Admin/CreateProduct.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import Layout from "./../../components/Layout/Layout";
+
+
+const CreateProduct = () => {
+  
+
+
+
+  return (
+    <Layout title={"Dashboard - Create Product"}>
+      
+    </Layout>
+  );
+};
+
+export default CreateProduct;
