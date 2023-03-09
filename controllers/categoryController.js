167c192a4d4b4e6dc34f00dbfe60b703fb089a5a
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-09 11:31:22 -0500

    add categoryController for deleting a category as an admin

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -124,3 +124,27 @@ export const singleCategoryController = async (req, res) => {
     });
   }
 };
+
+// Deleting a category (Admin)
+export const deleteCategoryController = async (req, res) => {
+  try {
+    
+    const { id } = req.params;
+    
+    await categoryModel.findByIdAndDelete(id);
+    
+    res.status(200).send({
+      success: true,
+      message: "Category Deleted Successfully",
+    });
+  } catch (error) {
+    
+    console.log(error);
+    
+    res.status(500).send({
+      success: false,
+      message: "There was an error while deleting the category",
+      error,
+    });
+  }
+};
