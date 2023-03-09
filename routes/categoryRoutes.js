f180f8403c177e8322b8195d0001533c83ac250c
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-09 10:01:56 -0500

    add a new route for creating a new category as an admin in categoryRoutes.js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -6,6 +6,18 @@ import { isAdmin, requireSignIn } from "./../middlewares/authMiddleware.js";
 const router = express.Router();
 
 
+// ROUTES
+
+
+// Creating a category (Admin)
+router.post(
+  "/create-category",
+  requireSignIn,
+  isAdmin,
+  createCategoryController
+);
+
+
 
 
 export default router;
