707af8152ff024a9be72e31c71dc3ecc7741547e
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-11 13:08:42 -0500

    import required packages/files in Products.js

--- client/src/pages/Admin/Products.js
+++ client/src/pages/Admin/Products.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import AdminMenu from "../../components/Layout/AdminMenu";
 import Layout from "./../../components/Layout/Layout";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
 
 
