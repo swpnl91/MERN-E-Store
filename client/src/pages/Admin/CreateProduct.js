4d54ee23c1b345503bd4c9685bb754100e4e9142
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-11 09:41:29 -0500

    import required files/packages in CreateProduct.js

--- client/src/pages/Admin/CreateProduct.js
+++ client/src/pages/Admin/CreateProduct.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./../../components/Layout/Layout";
 import AdminMenu from "./../../components/Layout/AdminMenu";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { Select } from "antd";
+import { useNavigate } from "react-router-dom";
+const { Option } = Select;
 
 
 
