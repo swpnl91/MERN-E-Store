3e035068ab73a249ccb7e9abc185f219020a4961
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-11 09:46:39 -0500

    add <AdminMenu /> to CreateProduct.js

--- client/src/pages/Admin/CreateProduct.js
+++ client/src/pages/Admin/CreateProduct.js
@@ -19,6 +19,10 @@ const CreateProduct = () => {
       <div className="container-fluid m-3 p-3 dashboard">
         <div className="row">
           
+          <div className="col-md-3">
+            <AdminMenu />
+          </div>
+
           
 
         </div>
