292c478bd15da98a33c4448fbed2b80142a70a2c
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-11 13:07:56 -0500

    create Products.js

---
+++ client/src/pages/Admin/Products.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import AdminMenu from "../../components/Layout/AdminMenu";
+import Layout from "./../../components/Layout/Layout";
+
+
+
+
+const Products = () => {
+  
+
+
+
+
+
+  return (
+    <Layout>
+      
+    </Layout>
+  );
+};
+
+export default Products;
