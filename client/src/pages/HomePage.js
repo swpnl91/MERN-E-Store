5a5bad9b02c58e77af3f143c0d5978a6301054d6
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-12 09:55:27 -0400

    add div to filter by category in HomePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -17,7 +17,30 @@ const HomePage = () => {
       />
       {/* banner image */}
 
-      
+      <div className="container-fluid row mt-3 home-page">
+        
+        <div className="col-md-3 filters">
+          
+          <h4 className="text-center">Filter By Category</h4>
+          
+          <div className="d-flex flex-column">
+            {categories?.map((c) => (
+              <Checkbox
+                key={c._id}
+                onChange={(e) => handleFilter(e.target.checked, c._id)}
+              >
+                {c.name}
+              </Checkbox>
+            ))}
+          </div>
+          
+          
+
+        </div>
+
+        
+
+      </div>
 
     </Layout>
   );
