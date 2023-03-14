c8ac66f4e95e5dbfc2d0b4207f4a5ed48b6d91e7
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-14 18:18:31 -0400

    import required files/packages in CartPage.js

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./../components/Layout/Layout";
+import { useCart } from "../context/cart";
+import { useAuth } from "../context/auth";
+import { useNavigate } from "react-router-dom";
+
+
+import axios from "axios";
+import toast from "react-hot-toast";
+
 
 
 
