9007f085f0939ac1fb53893feed91eed665052e6
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-14 17:20:29 -0400

    import required files/packages in CategoryProduct.js

--- client/src/pages/CategoryProduct.js
+++ client/src/pages/CategoryProduct.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../components/Layout/Layout";
+import { useParams, useNavigate } from "react-router-dom";
 
+import axios from "axios";
 
 
 
