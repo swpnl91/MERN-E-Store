c7b3e72f2848466bbe9e462a0db903d96056541f
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-14 14:49:27 -0400

    finish up useCategory hook

--- client/src/hooks/useCategory.js
+++ client/src/hooks/useCategory.js
@@ -1,9 +1,24 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-export default function useCategory() {
+export default function useCategory() {       // As per naming conventions we name this hook 'use...' 
   
-  
+  const [categories, setCategories] = useState([]);
+
+  // Function to get categories
+  const getCategories = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-categories");
+      setCategories(data?.category);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // useEffect for calling 'getCategories()'
+  useEffect(() => {
+    getCategories();
+  }, []);
 
   return categories;
 }
