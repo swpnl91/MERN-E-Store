9cc159080c512c58f140b6f9ab32110703c8c41d
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-14 12:25:38 -0400

    import ProductDetails in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import Profile from "./pages/user/Profile";
 import Products from "./pages/Admin/Products";
 import UpdateProduct from "./pages/Admin/UpdateProduct";
 import Search from "./pages/Search";
+import ProductDetails from "./pages/ProductDetails";
 
 
 
@@ -30,7 +31,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        
+        <Route path="/product/:slug" element={<ProductDetails />} />
 
         <Route path="/search" element={<Search />} />
 
