d16bac4613475b4713e345329b02c383bb37c6c1
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-14 12:01:13 -0400

    import required packages/files in ProductDetails.js

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./../components/Layout/Layout";
-
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
 
 
 
