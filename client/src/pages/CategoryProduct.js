ad949472394e3545bfa3724c7c80bd3ba4caa6d9
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-14 17:19:30 -0400

    create CategoryProduct.js

---
+++ client/src/pages/CategoryProduct.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import Layout from "../components/Layout/Layout";
+
+
+
+
+
+const CategoryProduct = () => {
+  
+  
+  
+
+
+  return (
+    <Layout>
+      
+    </Layout>
+  );
+};
+
+export default CategoryProduct;
