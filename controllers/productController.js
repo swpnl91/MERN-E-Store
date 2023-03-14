8b6fba92c119389932815dd40405c25da5a07071
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-14 14:15:34 -0400

    add relatedProductController in productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -349,3 +349,35 @@ export const searchProductController = async (req, res) => {
     });
   }
 };
+
+// Controller for getting similar products
+export const relatedProductController = async (req, res) => {
+  
+  try {
+    
+    const { pid, cid } = req.params;
+    
+    const products = await productModel
+      .find({
+        category: cid,
+        _id: { $ne: pid },
+      })
+      .select("-photo")
+      .limit(3)
+      .populate("category");
+    
+      res.status(200).send({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    
+    console.log(error);
+    
+    res.status(400).send({
+      success: false,
+      message: "Error while fetching similar products",
+      error,
+    });
+  }
+};
