f62a70c093b4378b55079f342009b009f07de18e
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-14 11:36:55 -0400

    finish up Search.js

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -12,6 +12,7 @@ const Search = () => {
   
   
   return (
+    
     <Layout title={"Search results"}>
       
       <div className="container">
@@ -25,7 +26,26 @@ const Search = () => {
               : `Found ${values?.results.length}`}
           </h6>
 
-          
+          <div className="d-flex flex-wrap mt-4">
+            {values?.results.map((p) => (        // We iterate through the 'values.results' array and render each product
+              <div className="card m-2" style={{ width: "18rem" }}>
+                <img
+                  src={`/api/v1/product/product-photo/${p._id}`}
+                  className="card-img-top"
+                  alt={p.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">
+                    {p.description.substring(0, 30)}...
+                  </p>
+                  <p className="card-text"> $ {p.price}</p>
+                  <button class="btn btn-primary ms-1">More Details</button>
+                  <button class="btn btn-secondary ms-1">ADD TO CART</button>
+                </div>
+              </div>
+            ))}
+          </div>
 
         </div>
       </div>
