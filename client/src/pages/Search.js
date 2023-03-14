15aa6c44edaec3b4aca32faca7b83b109e9cc5ab
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-14 11:33:12 -0400

    add headings to Search.js

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -17,6 +17,16 @@ const Search = () => {
       <div className="container">
         <div className="text-center">
           
+          <h1>Search Resuts</h1>
+          
+          <h6>
+            {values?.results.length < 1
+              ? "No Products Found"
+              : `Found ${values?.results.length}`}
+          </h6>
+
+          
+
         </div>
       </div>
 
