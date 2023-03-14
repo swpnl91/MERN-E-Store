5c7d6f16e48d1d7bc50858fed8f079fb5321c649
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-14 15:04:06 -0400

    create Categories.js

---
+++ client/src/pages/Categories.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+
+import Layout from "../components/Layout/Layout";
+
+
+const Categories = () => {
+  
+  
+  
+  return (
+    
+    <Layout title={"All Categories"}>
+      
+    </Layout>
+  );
+};
+
+export default Categories;
