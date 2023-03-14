d6b734f2da4073d9386454b6d318576a71580be4
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-14 12:23:23 -0400

    add section for similar products details in ProductDetails.js

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -70,11 +70,44 @@ const ProductDetails = () => {
                 alt={p.name}
               />
 
+              <div className="card-body">
+                <div className="card-name-price">
+                  
+                  <h5 className="card-title">{p.name}</h5>
+                  
+                  <h5 className="card-title card-price">
+                    {p.price.toLocaleString("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                    })}
+                  </h5>
+
+                </div>
+
+                <p className="card-text ">
+                  {p.description.substring(0, 60)}...
+                </p>
+
+                <div className="card-name-price">
+                  <button
+                    className="btn btn-info ms-1"
+                    onClick={() => navigate(`/product/${p.slug}`)}
+                  >
+                    More Details
+                  </button>
+
+                  {/* <button>
+                    ADD TO CART
+                  </button> */}
+
+                </div>
+                
+              </div>
               
             </div>
           ))}
         </div>
-        
+
       </div>
       
 
