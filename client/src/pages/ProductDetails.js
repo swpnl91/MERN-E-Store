386b70f69a5bdaf950027d8c73610415410eb5ea
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-14 12:17:23 -0400

    add section for product details in ProductDetails.js

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -28,7 +28,28 @@ const ProductDetails = () => {
           />
         </div>
 
-        
+        <div className="col-md-6 product-details-info">
+          
+          <h1 className="text-center">Product Details</h1>
+          
+          <hr />
+          
+          <h6>Name : {product.name}</h6>
+          
+          <h6>Description : {product.description}</h6>
+          
+          <h6>
+            Price :
+            {product?.price?.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })}
+          </h6>
+
+          <h6>Category : {product?.category?.name}</h6>
+          
+          <button class="btn btn-secondary ms-1">ADD TO CART</button>
+        </div>
 
       </div>
 
