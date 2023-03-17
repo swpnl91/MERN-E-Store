bd90fc06c9a3964bbc6fdf5304e646df37aa0042
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-17 12:45:50 -0400

    add getOrdersController in authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -246,3 +246,23 @@ export const updateProfileController = async (req, res) => {
     });
   }
 };
+
+// Get all orders as a User
+export const getOrdersController = async (req, res) => {
+  try {
+    const orders = await orderModel
+      .find({ buyer: req.user._id })
+      .populate("products", "-photo")
+      .populate("buyer", "name");
+    
+      res.json(orders);
+  } catch (error) {
+    console.log(error);
+    
+    res.status(500).send({
+      success: false,
+      message: "Error while getting orders",
+      error,
+    });
+  }
+};
