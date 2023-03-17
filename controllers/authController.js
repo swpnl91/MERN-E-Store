b2017a39df19da54f732ad3afab8527c54ec4add
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-03-17 17:27:19 -0400

    add orderStatusController in authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -290,3 +290,30 @@ export const getAllOrdersController = async (req, res) => {
     });
   }
 };
+
+// Updating order status as an Admin
+export const orderStatusController = async (req, res) => {
+  try {
+    
+    const { orderId } = req.params;
+    
+    const { status } = req.body;
+    
+    const orders = await orderModel.findByIdAndUpdate(
+      orderId,
+      { status },
+      { new: true }
+    );
+    
+    res.json(orders);
+  } catch (error) {
+    
+    console.log(error);
+    
+    res.status(500).send({
+      success: false,
+      message: "Error while updating order",
+      error,
+    });
+  }
+};
